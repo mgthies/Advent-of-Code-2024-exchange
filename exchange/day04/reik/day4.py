9cd18df13cce1fe989266ed225867221003c630e
Reik Keutterling <reik.keutterling@t-systems.com>
2024-12-05 00:11:02 +0100

    Reik: day3+day4 2024

---
+++ exchange/day04/reik/day4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+with open('day4-s.txt') as f:
+    input = {(x, y): char for y, line in enumerate(f) for x, char in enumerate(line.strip())}
+
+
+def find_word(matrix, word):
+    directions = [
+        (-1, 0), (1, 0), (0, -1), (0, 1),
+        (-1, -1), (-1, 1), (1, -1), (1, 1)
+    ]
+    for x, y in matrix:
+        for dx, dy in directions:
+            if all(matrix.get((x + dx * i, y + dy * i), '') == word[i] for i in range(len(word))):
+                yield x, y
+
+def find_cross(matrix):
+    for x, y in matrix:
+        if matrix.get((x, y), '') != 'A':
+            continue
+        pair1 = matrix.get((x - 1, y - 1), '') + matrix.get((x + 1, y + 1), '')
+        pair2 = matrix.get((x - 1, y + 1), '') + matrix.get((x + 1, y - 1), '')
+        if {pair1, pair2} <= {'MS', 'SM'}:
+            yield x, y
+            
+print('Part 1:', len(list(find_word(input, 'XMAS'))))
+print('Part 2:', len(list(find_cross(input))))
