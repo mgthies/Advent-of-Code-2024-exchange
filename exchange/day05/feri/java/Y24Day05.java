62b151277a744d53a30743b0173c03e37c7f3bd3
Ferenc Hechler <ferenc.hechler@telekom.de>
2024-12-05 09:11:20 +0100

    Feri day 5 in java

--- exchange/day05/feri/java/Y24Day05.java
+++ exchange/day05/feri/java/Y24Day05.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -69,6 +70,49 @@ public class Y24Day05 {
 
 	public static void mainPart2(String inputfile) throws FileNotFoundException {
 		
+		Map<Integer, Set<Integer>> beforeRules = new HashMap<>();
+		
+		try (Scanner scanner = new Scanner(new File(inputfile))) {
+			String line = scanner.nextLine().trim();
+			while (!line.isBlank()) {
+				int left = Integer.parseInt(line.split("[|]")[0]);
+				int right = Integer.parseInt(line.split("[|]")[1]);
+				beforeRules.computeIfAbsent(right, k->new HashSet<>()).add(left);
+				line = scanner.nextLine().trim();
+			}
+			System.out.println(beforeRules);
+			System.out.println();
+			int solution = 0;
+			while (scanner.hasNext()) {
+				line = scanner.nextLine().trim();
+				List<Integer> nums = Arrays.asList(line.split(",")).stream().map(s -> Integer.parseInt(s)).toList();
+				nums = new ArrayList<>(nums);
+				System.out.println(nums);
+				Set<Integer> afterNums = new HashSet<>(nums);
+				boolean corrected = false;
+				for (int i=0; i<nums.size(); i++) {
+					int num = nums.get(i); 
+					afterNums.remove(num);
+					Set<Integer> numsExpectedBefore = beforeRules.computeIfAbsent(num, k->Collections.emptySet());
+					Set<Integer> wrongAfterNums = new HashSet<>(afterNums);
+					wrongAfterNums.retainAll(numsExpectedBefore);
+					if (!wrongAfterNums.isEmpty()) {
+						nums.remove(i);
+						nums.add(num);
+						afterNums.add(num);
+						corrected = true;
+						i = i-1;
+						continue;
+					}
+				}
+				if (corrected) {
+					int median = nums.get(nums.size()/2);
+					solution += median;
+					System.out.println("  SORTED: "+nums+" median "+median+" SUM "+solution);
+				}
+			}
+			System.out.println("Solution: "+solution);
+		}
 	}
 
 
@@ -80,8 +124,8 @@ public class Y24Day05 {
 		System.out.println("---------------");
 		System.out.println();
 		System.out.println("--- PART II ---");
-		mainPart2("exchange/day05/feri/input-example.txt");
-//		mainPart2("exchange/day05/feri/input.txt");
+//		mainPart2("exchange/day05/feri/input-example.txt");
+		mainPart2("exchange/day05/feri/input.txt");
 		System.out.println("---------------");
 	}
 
